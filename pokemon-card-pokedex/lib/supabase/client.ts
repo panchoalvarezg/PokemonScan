e2893d81b9509bba01fed1b8ebb33ef71532bc75
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2026-04-17 00:37:44 -0400

    Refactor Supabase client creation function

--- pokemon-card-pokedex/lib/supabase/client.ts
+++ pokemon-card-pokedex/lib/supabase/client.ts
@@ -1,12 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export function createClient() {
-  return createClientBase();
-}
-
-function createClientBase() {
-  return createClient(supabaseUrl, supabaseAnonKey);
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
 }
