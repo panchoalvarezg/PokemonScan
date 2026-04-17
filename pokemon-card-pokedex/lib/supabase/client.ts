0532c3b15491d0654e133f493bcc8c502db0a3e6
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2026-04-17 00:37:27 -0400

    Update client.ts

--- pokemon-card-pokedex/lib/supabase/client.ts
+++ pokemon-card-pokedex/lib/supabase/client.ts
@@ -1,14 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-if (!supabaseUrl) {
-  throw new Error("Falta NEXT_PUBLIC_SUPABASE_URL");
+export function createClient() {
+  return createClientBase();
 }
 
-if (!supabaseAnonKey) {
-  throw new Error("Falta NEXT_PUBLIC_SUPABASE_ANON_KEY");
+function createClientBase() {
+  return createClient(supabaseUrl, supabaseAnonKey);
 }
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
